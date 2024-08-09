13aacba37073f64470027510606f7d6c94133c49
DereckJMichaud <michauddereck@gmail.com>
2024-08-09 12:59:37 -0400

    read file function

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world")
+def main():
+    with open("bookbot/books/frankenstein.txt") as frankenstein:
+        file_contents = frankenstein.read()
+        print(file_contents)
+
+main()
